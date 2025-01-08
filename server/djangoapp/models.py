304e1018d45a52f1809ce9fa5f6127a8aa416336
= <=>
2025-01-08 18:07:05 -0500

    Added Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -13,6 +12,26 @@
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    ORIGIN_CHOICES = [
+        ("DOMESTIC","Domestic"),
+        ("FOREIGN","Foreign")
+    ]
+    
+    name = models.CharField(
+        max_length=64
+    )
+    
+    description = models.TextField()
+    
+    origin = models.CharField(
+        max_length=8,
+        choices=ORIGIN_CHOICES,
+        default='0'
+    )
+
+    def __str__(self):
+        return f'Name: {self.Name}' 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +42,58 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    TYPE_CHOICES = [
+        ("COUPE","Coupe"),
+        ("CROSSOVER","Crossover"),
+        ("DELIVERY VAN","Delivery Van"),
+        ("HATCHBACK","Hatchback"),
+        ("MINIVAN","Minivan"),
+        ("PICKUP","Pickup"),
+        ("SEDAN","Sedan"),
+        ("SUV","SUV"),
+        ("WAGON","Wagon")
+    ]
+
+    DRIVETRAIN_CHOICES = [
+        ("DIESEL","Diesel"),
+        ("ELECTRIC","Electric"),
+        ("GAS","Gas"),
+        ("HYDROGEN","Hydrogen"),
+        ("MULTIFUEL","Mulitfuel")
+    ]
+
+    name = models.CharField(
+        max_length=64
+    )
+
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )
+
+    dealer_id = models.IntegerField()
+    
+    type = models.CharField(
+        max_length=12,
+        choices=TYPE_CHOICES,
+        default='SUV'
+    )
+
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+
+    drivetrain = models.CharField(
+        max_length=10,
+        choices=DRIVETRAIN_CHOICES,
+        default='ELECTRIC'
+    )
+
+    def __str__(self):
+        return f'Name: {self.Name}'
