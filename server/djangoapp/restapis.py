8501a920babeab97f1dcc27443f4698c586c423f
SmithsWaywardHome <150852590+SmithsWaywardHome@users.noreply.github.com>
2025-02-21 12:38:21 -0800

    Linted restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,42 +11,46 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # Get request to back end
-def get_request(endpoint, **kwargs):
+def get_request(endpoint, **kwargs): 
     params = ""
-    if(kwargs):
+    if(kwargs): 
         params = "?"
-        for key,value in kwargs.items():
+        for key,value in kwargs.items(): 
             params=params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+params
 
     print("GET from {} ".format(request_url))
-    try:
+    try: 
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
+    except Exception as err: 
         # If any error occurs
         print(f"Network exception occurred: {err}, {type(err)}")
 
+
 # Code for retrieving sentiments
-def analyze_review_sentiments(text):
+def analyze_review_sentiments(text): 
     request_url = sentiment_analyzer_url+"analyze/"+text
-    try:
+    try: 
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
+    except Exception as err: 
         print(f"Unexpected {err}, {type(err)}")
         print("Network exception occurred")
 
+
 # Code for posting review
-def post_review(data_dict):
+def post_review(data_dict): 
     request_url = backend_url+"/insert_review"
-    try:
-        response = requests.post(request_url,json=data_dict)
+    try: 
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err: 
+        print(f"Unexpected {err}, {type(err)}")
         print("Network exception occurred")
